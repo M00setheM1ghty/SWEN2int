fc6abfc377eaec8eac7b7abc0d4c6a6411f5affb
Baumann Thomas <tbaumann115@gmail.com>
2025-05-24 20:56:20 +0200

    added models and basic layout

---
+++ intermediateSWEN2/Models/Tour.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intermediateSWEN2.Models
+{
+    public class Tour : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public int Id { get; set; }
+
+        private string _name = "";
+        public string Name
+        {
+            get => _name;
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        private string _description = "";
+        public string Description
+        {
+            get => _description;
+            set { _description = value; OnPropertyChanged(); }
+        }
+
+        private string _from = "";
+        public string From
+        {
+            get => _from;
+            set { _from = value; OnPropertyChanged(); }
+        }
+
+        private string _to = "";
+        public string To
+        {
+            get => _to;
+            set { _to = value; OnPropertyChanged(); }
+        }
+
+        private string _transportType = "";
+        public string TransportType
+        {
+            get => _transportType;
+            set { _transportType = value; OnPropertyChanged(); }
+        }
+
+        public double Distance { get; set; } = 0.0;
+        public TimeSpan EstimatedTime { get; set; } = TimeSpan.Zero;
+        public string RouteImagePath { get; set; } = "placeholder.png";
+
+        public List<TourLog> Logs { get; set; } = new();
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
+
---
+++ intermediateSWEN2/Models/TourLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intermediateSWEN2.Models
+{
+    public class TourLog
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string? Comment { get; set; }
+        public string? Difficulty { get; set; }
+        public int TotalDistance { get; set; }
+        public TimeSpan TotalTime { get; set; }
+        public int Rating { get; set; }
+
+    }
+}
